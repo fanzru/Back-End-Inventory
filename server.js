52a90cb7242e6c4c9b48005da2268180ef02ed4d
Fanzru <fattahilaaf080701@gmail.com>
2021-07-19 11:36:12 +0700

    set header

--- server.js
+++ server.js
@@ -24,6 +24,11 @@ app.use(express.urlencoded({extended:true}));
 //    saveUnitializied: false
 //  })
 //)
+app.use((req,res, next)=>{
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
+})
 app.use('/user',authRoute);
 
 // Database Access
