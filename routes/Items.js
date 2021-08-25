d9ef2d3aa9ef2077d0f0b2123517f096b1878dce
Fanzru <fattahilaaf080701@gmail.com>
2021-08-25 20:21:02 +0700

    add commit

--- routes/Items.js
+++ routes/Items.js
@@ -1,108 +1,130 @@
-const router     = require('express').Router();
-const ITEMS      = require('../models/Item');
-const BORROWER   = require('../models/borrower')
-const {response} = require('../controllers/response')
+const router = require('express').Router()
+const ITEMS = require('../models/Item')
+const Category = require('../models/category')
+const BORROWER = require('../models/borrower')
+const { response } = require('../controllers/response')
 // Get all Data
 const error = null
-router.get('/',async (req,res)=>{
-    try {
-        const items = await ITEMS.find();
-        response(res,true,items,'Get All Items Succes',200)
-    } catch{
-        response(res,false,error,'Item Not Found',400)
-    }
+router.get('/', async (req, res) => {
+  try {
+    const items = await ITEMS.aggregate([
+      {
+        $lookup: {
+          from: Category.collection.name,
+          let: {
+            categoryId: { $toObjectId: '$categoryId' },
+          },
+          as: 'detailCategory',
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $eq: ['$$categoryId', '$_id'],
+                },
+              },
+            },
+          ],
+        },
+      },
+    ])
+
+    response(res, true, items, 'Get All Items Succes', 200)
+  } catch (err) {
+    console.log(err)
+    response(res, false, error, 'Item Not Found', 400)
+  }
 })
 // Input New item
-router.post('/inputnewItem',async (req,res)=>{
-    let kode = "BRG-"
-    let itemCode = kode + 0
-    var lastDoc = await ITEMS.find().sort({_id: -1}).limit(1);
-    
-    if (lastDoc.length != 0){
-        var code = (lastDoc[0].itemCode).split("-")
-        var noid = parseInt(code[1]) + 1
-        itemCode = kode +  noid
-    }
-    
-    const itemExist = await ITEMS.findOne({itemName: req.body.itemName});
-    if (itemExist) return response(res,false,lastDoc,'Item Already Exist',400)
+router.post('/inputnewItem', async (req, res) => {
+  let kode = 'BRG-'
+  let itemCode = kode + 0
+  var lastDoc = await ITEMS.find().sort({ _id: -1 }).limit(1)
 
-    const newItem = new ITEMS({
-        categoryId: req.body.categoryId,
-        itemName: req.body.itemName,
-        itemCode: itemCode,
-        itemAmount: req.body.itemAmount,
-        itemInBorrow: 0,
-    })
-    
-    try{
-        const savedItem = await newItem.save();
-        response(res,true,savedItem,'Input Item Succes',200)
-    } catch {
-        response(res,false,error,'Input Item Failed',400)
-    }
-    
+  if (lastDoc.length != 0) {
+    var code = lastDoc[0].itemCode.split('-')
+    var noid = parseInt(code[1]) + 1
+    itemCode = kode + noid
+  }
+
+  const itemExist = await ITEMS.findOne({ itemName: req.body.itemName })
+  if (itemExist) return response(res, false, lastDoc, 'Item Already Exist', 400)
+
+  const newItem = new ITEMS({
+    categoryId: req.body.categoryId,
+    itemName: req.body.itemName,
+    itemCode: itemCode,
+    itemAmount: req.body.itemAmount,
+    itemInBorrow: 0,
+  })
+
+  try {
+    const savedItem = await newItem.save()
+    response(res, true, savedItem, 'Input Item Succes', 200)
+  } catch {
+    response(res, false, error, 'Input Item Failed', 400)
+  }
 })
 // get item using id item
-router.get('/findItem/:itemid',async (req,res)=>{
-    try {
-        const Item = await ITEMS.findOne({_id: req.params.itemid});
-        if (Item != null) return response(res,true,Item,'Search Data Succes',200)
-    } catch {
-        response(res,false,Item,`${req.params.itemid} Not Found`,400)
-    }
+router.get('/findItem/:itemid', async (req, res) => {
+  try {
+    const Item = await ITEMS.findOne({ _id: req.params.itemid })
+    if (Item != null)
+      return response(res, true, Item, 'Search Data Succes', 200)
+  } catch {
+    response(res, false, Item, `${req.params.itemid} Not Found`, 400)
+  }
 })
 // rename item using item id
-router.post('/renameItem/:itemid',async (req,res)=>{
-    try {
-        const updateItem = await ITEMS.updateOne(
-            {_id: req.params.itemid}, 
-            {$set:{itemName: req.body.itemName}
-        });
-        response(res,true,updateItem,'Update Success',200)
-    } catch {
-        response(res,false,error,`${req.params.itemid} Not Found`,400)
-    }
+router.post('/renameItem/:itemid', async (req, res) => {
+  try {
+    const updateItem = await ITEMS.updateOne(
+      { _id: req.params.itemid },
+      { $set: { itemName: req.body.itemName } },
+    )
+    response(res, true, updateItem, 'Update Success', 200)
+  } catch {
+    response(res, false, error, `${req.params.itemid} Not Found`, 400)
+  }
 })
 // delete item using item id
-router.delete('/deleteItem/:itemid', async (req,res)=>{
-    try{
-        const dataBorrower = await BORROWER.findOne({itemId: req.params.itemid});
-        if (dataBorrower.length == 0) return response(res,false,error,'delete item failed',400)
-        
-        const deleteItem = await ITEMS.remove({_id: req.params.itemid});
-        response(res,true,deleteItem,'delete item success',200)
-    } catch{
-        response(res,false,error,'delete item failed',400)
-    }
+router.delete('/deleteItem/:itemid', async (req, res) => {
+  try {
+    const dataBorrower = await BORROWER.findOne({ itemId: req.params.itemid })
+    if (dataBorrower.length == 0)
+      return response(res, false, error, 'delete item failed', 400)
+
+    const deleteItem = await ITEMS.remove({ _id: req.params.itemid })
+    response(res, true, deleteItem, 'delete item success', 200)
+  } catch {
+    response(res, false, error, 'delete item failed', 400)
+  }
 })
 
-router.post('/minItem/:itemid/:amountitem', async (req,res)=> {
-    
-    try{
-        const item = await ITEMS.findOne({_id: req.params.itemid});
-        console.log(typeof(item.itemAmount),typeof(itemAmount))
-        if ( (item.length != 0) &&  (item.itemAmount >= itemAmount) ){
-            item.itemAmount   -= parseInt(req.params.amountitem)
-            const savedItem = await item.save();
-            response(res,true,savedItem,'Min Item Succes',200)
-        }
-    } catch {
-        response(res,false,error,'Min Item Failed',400)
+router.post('/minItem/:itemid/:amountitem', async (req, res) => {
+  try {
+    const item = await ITEMS.findOne({ _id: req.params.itemid })
+    console.log(typeof item.itemAmount, typeof itemAmount)
+    if (item.length != 0 && item.itemAmount >= itemAmount) {
+      item.itemAmount -= parseInt(req.params.amountitem)
+      const savedItem = await item.save()
+      response(res, true, savedItem, 'Min Item Succes', 200)
     }
+  } catch {
+    response(res, false, error, 'Min Item Failed', 400)
+  }
 })
 
-router.post('/addItem/:itemid/:amountitem', async (req,res)=> {
-    try{
-        const item = await ITEMS.findOne({_id: req.params.itemid});
-        if ( (item.length != 0 ) &&  ( 1 <= req.params.amountitem)){
-            item.itemAmount  += parseInt(req.params.amountitem)
-            const savedItem = await item.save();
-            response(res,true,savedItem,'Return Item Succes',200)
-        }
-    } catch {
-        response(res,false,error,'item not found',400)
+router.post('/addItem/:itemid/:amountitem', async (req, res) => {
+  try {
+    const item = await ITEMS.findOne({ _id: req.params.itemid })
+    if (item.length != 0 && 1 <= req.params.amountitem) {
+      item.itemAmount += parseInt(req.params.amountitem)
+      const savedItem = await item.save()
+      response(res, true, savedItem, 'Return Item Succes', 200)
     }
+  } catch {
+    response(res, false, error, 'item not found', 400)
+  }
 })
 
-module.exports = router;
+module.exports = router
